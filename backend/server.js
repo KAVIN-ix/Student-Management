274fedc731610044afb9cc08787e3dc328bec2bc
KAVIN-ix <your-2503717620521305@cit.edu.in>
2026-05-24 19:15:31 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+
+const connectDB = require("./config/db");
+
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+
+// Custom Middleware
+app.use((req, res, next) => {
+  console.log(req.method);
+  next();
+});
+
+
+app.use("/students", require("./routes/studentRoutes"));
+
+app.listen(5000, () => {
+  console.log("Server Running on Port 5000");
+});
